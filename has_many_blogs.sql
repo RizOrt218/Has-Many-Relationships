97d31aba0f3ad6f625399b66de4bd0549c3bedf7
RizOrt218 <rizhiafalen@gmail.com>
2016-01-21 19:03:11 -1000

    fixes timestamp for users table

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -11,3 +11,17 @@ CREATE USER has_many_user;
 CREATE DATABASE has_many_blogs;
 ALTER DATABASE has_many_blogs OWNER TO has_many_user;
 
+--connects to database
+\c has_many_blogs;
+
+CREATE TABLE IF NOT EXISTS users
+(
+  id SERIAL NOT NULL,
+  username varchar(90) NOT NULL,
+  first_name varchar(90) NOT NULL,
+  last_name varchar(90) NULL DEFAULT NULL,
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp NOT NULL DEFAULT now()
+);
+
+
